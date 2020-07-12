d5afe74bfbb08e4a6c2b9011e20b8207d3e8de14
Paramesh Vijay <parameshvijay@yandex.com>
2020-07-12 15:54:52 +0530

    Initial Setup completed

--- AppAutomation/src/main/java/commonRepo/CommonObjects.java
+++ AppAutomation/src/main/java/commonRepo/CommonObjects.java
@@ -3,35 +3,37 @@ package commonRepo;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-
-import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.BeforeClass;
 
 public class CommonObjects {
 
 	public static DesiredCapabilities caps;
 	public static WebDriverWait wait;
-	public static AndroidDriver<WebElement> driver;
+	public static RemoteWebDriver driver;
 
-	@BeforeSuite
-	public void setup() throws MalformedURLException {
+	@BeforeClass
+	public static void setup() throws InterruptedException {
+
+		// Set the Desired Capabilities
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability("deviceName", "Honor 9 Lite");
 		caps.setCapability("udid", "9SF3Y18628012287"); // Give Device ID of your mobile phone
 		caps.setCapability("platformName", "Android");
 		caps.setCapability("platformVersion", "9");
+		caps.setCapability("appPackage", "com.flipkart.android");
+		caps.setCapability("appActivity", ".activity.HomeFragmentHolderActivity");
 		caps.setCapability("noReset", "true");
-		caps.setCapability("appPackage", "com.android.chrome");
-		caps.setCapability("appActivity", "com.google.android.apps.chrome.Main");
 
-		driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4725/wd/hub"), caps);
+		try {
+			driver = new RemoteWebDriver(new URL("http://0.0.0.0:4725/wd/hub"), caps);
 
-		wait = new WebDriverWait(driver, 30);
-		driver.get("https://www.flipkart.com/");
+		} catch (MalformedURLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@AfterSuite
---
+++ AppAutomation/src/test/java/com/flipkart/homePage/Flipkart.java
@@ -0,0 +1,11 @@
+package com.flipkart.homePage;
+
+import org.testng.annotations.Test;
+
+public class Flipkart {
+
+	@Test
+	public void homePage() {
+
+	}
+}
